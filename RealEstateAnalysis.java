b6e360b9a54f1d6d0873c9a9c2906f0754d4d608
hdwyerr <hedwyer20@gmail.com>
2022-10-30 13:50:56 -0400

    created .java file for my project and did pseudocode for my 3 questions

---
+++ RealEstateAnalysis.java
@@ -0,0 +1,28 @@
+//QUESTION 1: WHAT IMPACT DOES A HOUSES LOCATION HAVE ON ITS LIST PRICE?
+    //hard code a list of small and large cities as a way to make rural and urban categories
+    //grab first line of data set and make a header array
+    //make ruralHomePrices AL and urbanHomePrices AL 
+    //go through header array and find locationIndex and priceIndex
+    //use a while loop to go through each entry and look at its location
+        //if it is a small city, add its price to the ruralHomePrices AL
+        //if it is a large city, add its price to the urbanHomePrices AL
+        //if its location doesn't fall in my hard coded list, ignore that enry
+    //once the ArrayLists are completed with all the rural home prices and urban home prices, use average method to find the average of each AL
+    //compare the two
+
+
+//QUESTION 2: WHAT IMPACT DOES THE AMOUNT OF INVENTORY HAVE ON LIST PRICE?
+    //randomly select 5 cities
+    //make inventoryIndex variagble
+    //for each city, use invnetortyIndex to find amount of homes for sale
+    //make cityHomePrices AL
+    //use while loop to go through data and add for that particular city, add the price of each home to the AL
+    //find average of home prices, and display it alongside inventory
+    //repeat for each city, and then look at the results and see if any conclusions can be made
+
+
+//QUESTION 3: IS THERE A CORRELATION BETWEEN HOME PRICE AND THE AMOUNT OF TIME THE HOUSE TAKES TO SELL?
+    //randomly select 5 houses
+    //create two parallel ArrayLists, homePrice and sellingTime
+    //for each house, use a while loop to go through the data and add its price and selling time to the corresponding AL
+    //compare prices and selling time and see if there are any trends 
