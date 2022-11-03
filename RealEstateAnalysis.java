a28a368df095d80507bd4cf70382e55464d187be
hdwyerr <hedwyer20@gmail.com>
2022-11-03 11:42:21 -0400

    final code with comments for submission

--- RealEstateAnalysis.java
+++ RealEstateAnalysis.java
@@ -6,6 +6,7 @@ public class RealEstateAnalysis{
         citiesVsRuralPrices();
         mostAndLeastExpensiveStates();
         mostExpensiveCoast();
+        //main method running methods for each question
     }
 
 
